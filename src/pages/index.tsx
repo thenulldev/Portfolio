fd0a3b037eb68800d2032d580a62adfddf6b3afb
Stephen Freerking <snipeydev@gmail.com>
2022-12-07 08:16:24 -0800

    Fixed css and links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -56,7 +56,7 @@ export function Repos({
         <h1 className="pb-8 pl-5 text-2xl font-light text-left text-white -pt-8">
           Projects
         </h1>
-        <div className="flex flex-wrap -m-4">
+        <div className="flex flex-wrap justify-center -m-4">
           {/* {JSON.stringify(pinned)} */}
           {pinned &&
             pinned.map((repo, index) => (
@@ -64,39 +64,41 @@ export function Repos({
                 key={index}
                 className="w-full p-4 lg:w-1/3 md:w-1/2 xl:w-1/4"
               >
-                <div className="h-full overflow-hidden border-2 border-gray-200 rounded-lg bg-slate-100 border-opacity-60">
-                  <div className="flex flex-col justify-between h-full p-6 ">
-                    <div>
-                      <h2 className="mb-1 text-xs font-medium tracking-widest text-gray-400 title-font">
-                        {repo.primaryLanguage.name}
-                      </h2>
-                      <h1 className="mb-3 text-lg font-medium title-font text-slate-800">
-                        <Link href={repo.url}>{repo.name}</Link>
-                      </h1>
-                      <p className="mb-3 leading-relaxed text-slate-800">
-                        {repo.description}
-                      </p>
+                <div className="h-full overflow-hidden transition border-2 border-gray-200 rounded-lg bg-slate-100 hover:bg-white border-opacity-60">
+                  <Link href={repo.url}>
+                    <div className="flex flex-col justify-between h-full p-6">
+                      <div className="flex-grow">
+                        <h2 className="mb-1 text-xs font-medium tracking-widest text-gray-400 title-font">
+                          {repo.primaryLanguage.name}
+                        </h2>
+                        <h1 className="mb-3 text-lg font-medium title-font text-slate-800">
+                          {repo.name}
+                        </h1>
+                        <p className="mb-3 leading-relaxed text-slate-800">
+                          {repo.description}
+                        </p>
+                      </div>
+                      <div className="flex flex-wrap items-center">
+                        <a className="inline-flex items-center text-indigo-500 md:mb-2 lg:mb-0"></a>
+                        <span className="inline-flex py-1 pr-3 ml-auto mr-3 text-sm leading-none border-r-2 border-gray-200 text-slate-500 dark:text-gray-400 lg:ml-auto md:ml-0">
+                          <FontAwesomeIcon
+                            className="pr-1 hover:fill-blue-600"
+                            icon={faStar}
+                            size="sm"
+                          />
+                          {repo.stargazerCount}
+                        </span>
+                        <span className="inline-flex text-sm leading-none text-slate-500 dark:text-gray-400">
+                          <FontAwesomeIcon
+                            className="pr-1"
+                            icon={faCodeFork}
+                            size="sm"
+                          />
+                          {repo.forkCount}
+                        </span>
+                      </div>
                     </div>
-                    <div className="flex flex-wrap items-center">
-                      <a className="inline-flex items-center text-indigo-500 md:mb-2 lg:mb-0"></a>
-                      <span className="inline-flex py-1 pr-3 ml-auto mr-3 text-sm leading-none border-r-2 border-gray-200 text-slate-500 dark:text-gray-400 lg:ml-auto md:ml-0">
-                        <FontAwesomeIcon
-                          className="pr-1 hover:fill-blue-600"
-                          icon={faStar}
-                          size="sm"
-                        />
-                        {repo.stargazerCount}
-                      </span>
-                      <span className="inline-flex text-sm leading-none text-slate-500 dark:text-gray-400">
-                        <FontAwesomeIcon
-                          className="pr-1"
-                          icon={faCodeFork}
-                          size="sm"
-                        />
-                        {repo.forkCount}
-                      </span>
-                    </div>
-                  </div>
+                  </Link>
                 </div>
               </div>
             ))}
